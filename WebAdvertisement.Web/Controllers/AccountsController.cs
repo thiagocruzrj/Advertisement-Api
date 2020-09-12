155f35fbca55e0bc2997256022ab0bafe27e16c3
Thiago <thagocruz@gmail.com>
2020-09-12 18:25:22 -0300

    Add Signup model

---
+++ WebAdvertisement.Web/Controllers/AccountsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAdvertisement.Web.Controllers
+{
+    public class AccountsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebAdvertisement.Web/Models/Accounts/SignupViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAdvertisement.Web.Models.Accounts
+{
+    public class SignupViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(6, ErrorMessage = "Password must be at least 6 characters long!")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password and confirm password do not match!")]
+        public string ConfirmPassword { get; set; }
+    }
+}
