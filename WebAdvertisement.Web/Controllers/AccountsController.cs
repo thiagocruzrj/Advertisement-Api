c403c4b2de04ecb3dca860cd26c40f2f4718d53d
Thiago <thagocruz@gmail.com>
2020-09-12 20:54:56 -0300

    Add account constructor with cognito parameters

--- WebAdvertisement.Web/Controllers/AccountsController.cs
+++ WebAdvertisement.Web/Controllers/AccountsController.cs
@@ -1,10 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Amazon.Extensions.CognitoAuthentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebAdvertisement.Web.Models.Accounts;
 
 namespace WebAdvertisement.Web.Controllers
 {
     public class AccountsController : Controller
     {
-        public IActionResult Index()
+        private readonly SignInManager<CognitoUser> _signInManager;
+        private readonly UserManager<CognitoUser> _userManager;
+        private readonly CognitoUserPool _pool;
+
+        public AccountsController(SignInManager<CognitoUser> signInManager, UserManager<CognitoUser> userManager, CognitoUserPool pool)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _pool = pool;
+        }
+
+        public async Task<IActionResult> Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Signup(SignupViewModel signup)
         {
             return View();
         }
